70e042750f99f87888076411cea94d4fdda1611a
Oleggood <oleggood@mail.ru>
2023-01-16 13:21:24 +0300

    стабильный минимум

--- app/Http/Controllers/ExecutorController.php
+++ app/Http/Controllers/ExecutorController.php
@@ -21,10 +21,12 @@ class ExecutorController extends Controller
     public function show($user)
     {
         $oneUser = User::where('id', $user)->with('tasks')->get();
+        //$tasksUser = $oneUser->tasks;
 
         return view('public.executor.show', [
             'main_header' => 'Задачи пользователя',
             'oneUser' => $oneUser,
+            //'tasksUser' => $tasksUser,
             'color_text' => 'color:red',
         ]);
     }
--- resources/views/public/executor/show.blade.php
+++ resources/views/public/executor/show.blade.php
@@ -1,7 +1,21 @@
 <h1>{{$main_header}}</h1>
 
+
+<a href="{{ route('executor.index') }}">Назад к списку</a>
+
+
 @foreach($oneUser as $user)
+
     <p>{{$user->surname}} {{$user->name}} {{$user->patronymic}}</p>
+
+    <p>Отображать статусы:</p> {{-- todo - фильтр --}}
+    <form action="" method="get">
+        @foreach($user->tasks as $userTasks)
+            <input type="checkbox" name="{{$userTasks->status->id}}">{{$userTasks->status->status}}
+        @endforeach
+            <input type="submit" value="Фильтровать">
+    </form>
+
     <table>
         <tr>
             <th>№</th>
@@ -15,24 +29,24 @@
             <th>Текущий статус</th>
         </tr>
         @foreach($user->tasks as $userTasks)
-        <tr>
-            <td>{{$loop->iteration}}</td>
-            <td>{{date('d.m.Y', strtotime($userTasks->task_date))}}</td>
-            <td>{{$userTasks->number}}</td>
-            <td>{{$userTasks->task_name}}</td>
-            <td>{{$userTasks->item}}</td>
-            <td>{{$userTasks->task}}</td>
-            <td><i>{{$userTasks->note}}</i></td>
-            <td>
+            <tr>
+                <td>{{$loop->iteration}}</td>
+                <td>{{date('d.m.Y', strtotime($userTasks->task_date))}}</td>
+                <td>{{$userTasks->number}}</td>
+                <td>{{$userTasks->task_name}}</td>
+                <td>{{$userTasks->item}}</td>
+                <td>{{$userTasks->task}}</td>
+                <td><i>{{$userTasks->note}}</i></td>
+                <td>
                     <span style="
                         @if ($userTasks->deadline <= today())
                            {{$color_text}}
                         @endif
                         ">{{date('d.m.Y', strtotime($userTasks->deadline))}}
                     </span>
-            </td>
-            <td>{{$userTasks->status->status}}</td>
-        </tr>
+                </td>
+                <td>{{$userTasks->status->status}}</td>
+            </tr>
 
         @endforeach
     </table>
